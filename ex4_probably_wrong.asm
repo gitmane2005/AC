ea3ce8ac1c26576dc45e84748185238b5cb94fdc
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-02-21 19:23:31 +0000

    Add files via upload

---
+++ ex4_probably_wrong.asm
@@ -0,0 +1,26 @@
+data
+num: .word 7
+.text
+la t0, num
+lw a0, 0(t0)
+jal ra, POLI
+li a7, 10
+ecall
+POLI:
+       addi sp, sp, -16
+       sw ra, 12(sp)
+       sw a0, 8(sp)
+       mv t0, a0
+       jal ra, QUAD
+       mv t0, a0
+       li a0,3
+       mul s0, s0, a0
+       add a0, t0, s0
+       addi a0, a0, 1
+       lw s0, 0(sp)
+       lw ra, 4(sp)
+       addi sp, sp, 8
+       ret
+QUAD:
+       mul a0, a0, a0
+       ret
