3b96faf8cac15ee4eedbf0c1cad9520e8ab0ceaf
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-03-07 20:29:46 +0000

    update

--- ficha 1/ex8_b.asm
+++ ficha 1/ex8_b.asm
@@ -1,20 +1,34 @@
 .data 
 v1: .word 10, 20, 30, 40, 50
-v2: .word 2, 4, 6, 8, 30
+v2: .word 2, 4, 6, 8, 234567
 n: .word 5
 .text
 la a0, v1
-la a1, v2
-lw a2, n
-jal ra, biggest
+la a2, v2
+lw a1, n
+jal ra, MAXMED
 li a7, 10
 ecall 
 
 .text
 MAXMED: 
-	lw t2, 0(a2)
-	mv t3, zero
-	mv t4, zero
+	addi sp, sp, -16
+	sw ra, 12(sp)
+	sw a2, 8(sp)
+	jal ra, MEDIA
+	sw a0, 4(sp)
+	lw a0, 8(sp)
+	jal ra, MEDIA
+	lw t0, 4(sp)
+	lw ra, 12(sp)
+	addi sp, sp, 16
+	bgt t0, a0, retorno
+	ret
+	
+retorno:
+	mv a0, t0
+	ret
+
 
 
 
@@ -36,4 +50,5 @@ MEDIA:
 	jal ra, soma
 	div a0, a0, a1
 	lw ra, 12(sp)
+	addi sp, sp, 16
 	ret
---
+++ ficha dois/ex10_b.asm
@@ -0,0 +1,25 @@
+.data
+V: .word 10, 3, 38, 8493, 3, 2, 2,2,2,2,2,2
+n: .word 12
+lim: .word 2
+
+.text 
+la a0, V
+lw a1, n
+lw a2, lim
+jal conta_ocorr
+li a7, 10
+ecall 
+
+conta_ocorr:
+	addi t0, x0, 0
+ciclo:	beqz a1, fim
+	lw t1, 0(a0)
+	addi a0, a0, 4
+	addi a1, a1, -1
+	bne  a2, t1, ciclo
+	addi t0, t0, 1
+	j ciclo
+
+fim:	mv a0, t0
+	ret 
